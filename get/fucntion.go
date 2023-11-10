ffb09f8164fd2502dccfe6335d64e4a2180c3dc4
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-11-10 19:43:48 +0700

    updated link repo

--- get/fucntion.go
+++ get/fucntion.go
@@ -13,6 +13,6 @@ func init() {
 }
 
 func petaSalGet(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "https://muhaammad-faisal-ashshidiq.github.io/")
+	w.Header().Set("Access-Control-Allow-Origin", "https://nugisorange.github.io/")
 	fmt.Fprintf(w, petback.GCFHandler("MONGOJAMBE", "petasalget", "petback"))
 }
--- post/fucntion.go
+++ post/fucntion.go
@@ -15,7 +15,7 @@ func init() {
 func petbackPost(w http.ResponseWriter, r *http.Request) {
     // Set CORS headers for the preflight request
     if r.Method == http.MethodOptions {
-        w.Header().Set("Access-Control-Allow-Origin", "https://muhammad-faisal-ashshidiq.github.io")
+        w.Header().Set("Access-Control-Allow-Origin", "https://nugisorange.github.io")
         w.Header().Set("Access-Control-Allow-Methods", "POST")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
         w.Header().Set("Access-Control-Max-Age", "3600")
@@ -23,7 +23,7 @@ func petbackPost(w http.ResponseWriter, r *http.Request) {
         return
     }
     // Set CORS headers for the main request.
-    w.Header().Set("Access-Control-Allow-Origin", "https://muhammad-faisal-ashshidiq.github.io")
+    w.Header().Set("Access-Control-Allow-Origin", "https://nugisorange.github.io")
     fmt.Fprintf(w, petback.GCFPostHandler("PASETOPRIVATEKEYENV" ,"MONGOSTRING", "petback", "tesgis", r))
 
 }
